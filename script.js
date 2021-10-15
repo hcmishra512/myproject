f34eba6f31b884f81f620cca729166b04c329217
hcmishra512 <h.c.mishra512@gmail.com>
2021-10-15 23:12:51 +0530

    random change

---
+++ script.js
@@ -0,0 +1,184 @@
+// const firstName = "Harish";
+// const job = "Engineer";
+// const birthYear = "1977";
+// const year = "2021";
+// const harish ="I'm " +  firstName + ', a ' + ( year-birthYear ) + ' year old ' +  job + '!';
+// console.log(harish);
+// console.log('Just a regular Strings...')
+
+// -----------Example 1------------
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn =1.95;
+// const BMIMark = massMark/heightMark**2;
+// const BMIJohn = massJohn/heightJohn**2;
+// const heigherMarkBMI = BMIMark>BMIJohn;
+// console.log(BMIMark, BMIJohn, heigherMarkBMI)
+
+
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn =1.76;
+// const BMIMark = massMark/heightMark**2;
+// const BMIJohn = massJohn/heightJohn**2;
+// const heigherMarkBMI = BMIMark>BMIJohn;
+// console.log(BMIMark, BMIJohn, heigherMarkBMI)
+
+
+
+// const age = 20;
+// const isOldEnough = age >= 18;
+// if(isOldEnough) {
+//     console.log('Sarah can start driving License');
+// }
+
+//  -----------if & else control structure------------
+
+// const age = 15;
+// if (age >= 18) {
+//     console.log('Sarah can start driving license');
+// } else {
+//     const yearLeft = 18 - age;
+//     console.log('Sarah is too young, wait another '+(18 - age)+' years :)' );
+// }
+
+// const birthYear = 1999;
+// let century;
+// if (birthYear <= 2000) {
+//  century = 20;
+// } else{
+//  century =21;
+// }
+// console.log(century);
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn =1.95;
+// const BMIMark = massMark / heightMark**2;
+// const BMIJohn = massJohn / heightJohn**2;
+// const heigherMarkBMI = BMIMark > BMIJohn;
+// console.log(BMIMark, BMIJohn)
+// if (BMIMark > BMIJohn){
+//     console.log("Mark's BMI (${BMIMark}) is higher than John's !(${BMIJohn})");
+// }else {
+//     console.log("John's BMI is higher than Mark's !");
+// }
+
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn =1.76;
+// const BMIMark = massMark/heightMark**2;
+// const BMIJohn = massJohn/heightJohn**2;
+// const heigherMarkBMI = BMIMark>BMIJohn;
+// console.log(BMIMark, BMIJohn);
+
+// if (BMIMark > BMIJohn){
+//     console.log("Mark's BMI is higher than John's !");
+// }else {
+//     console.log("John's BMI is higher than Mark's !");
+// }
+
+// const inputYear = '1991';
+// console.log(inputYear +18);
+
+// const inputYear = 1991;
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number('Jonas'));
+// console.log(String(23), 23);
+
+// console.log('I am ' + 23 +' year old');
+// console.log('23'-'10'-3);
+// console.log('23'* '2');
+// console.log('23'/ '2');
+// console.log('23' > '28');
+                 
+// let n ='1'+ 1;      // there is string & Number. add = 11
+//     n = n - 1;
+//     console.log(n);
+
+    
+//     // const money = 0;
+//     // if (money) {
+//     //     console.log("Don't spend it all:)");
+//     // }else {
+//     //     console.log('you should get a job!');
+//     // }
+
+//     const money = 10;
+//     if (money) {
+//         console.log("Don't spend it all:)");
+//     }else {
+//         console.log('you should get a job!');
+//     }
+
+    // let height;
+    // if (height){
+    //     console.log('YAY! Height is Defined');
+    // } else {
+    //     console.log('Height is UNDEFINED');
+    // }
+
+    // let height = 100;
+    // if (height){
+    //     console.log('YAY! Height is Defined');
+    // } else {
+    //     console.log('Height is UNDEFINED');
+    // }
+    
+  /*    var a= 10;
+        var b= 24;
+        var c= a+b;
+        var d= a<b;
+        document.write(c);
+        document.write("<br>");
+        document.write(d);  */
+       
+     /* var a= 11;
+        var b= 10;
+        if(a>b) {
+            document.write("its a true");
+        }
+        else {
+            document.write("its a not true");
+        }
+        */
+
+      /*  var a=30;
+        var b=20;
+        if(a>b) {
+            document.write("It is write");
+        }
+        else if(a==b){
+            document.write("It is a equal");
+        }
+        else{
+            document.write("wrong")
+        }
+        */
+       /* var count =5;
+        while(count<10){
+            document.write("Harish Chandra Mishra<br>")
+            count++;
+        }
+        */
+    //     var count=10;
+    //     for(var i=0; i<count; i++){
+    //         document.write("H C Mishra<br>")
+    //     }
+  const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn =1.76;
+const BMIMark = massMark/heightMark**2;
+const BMIJohn = massJohn/heightJohn**2;
+const heigherMarkBMI = BMIMark>BMIJohn;
+console.log(BMIMark, BMIJohn);
